ffbdb26659c5f94f2a0fe8f4d45d6c8dd35dd14f
Artem Faradey <artemfap@yandex.ru>
2020-05-15 01:13:34 +0300

    commit 2

--- python_repos.py
+++ python_repos.py
@@ -1,5 +1,6 @@
 import requests
 import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 
 url = 'https://api.github.com/search/repositories?q=tetris&sort=stars'
@@ -12,13 +13,18 @@ print("Total repositories: ", response_dict['total_count'])
 
 # Анализ информации о репозиториях
 repo_dicts = response_dict['items']
-print("Repositories returned: ", len(repo_dicts))
-print("\nSelected information about each repository:")
+
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('\nName: ', repo_dict['name'])
-    print('Owner: ', repo_dict['owner']['login'])
-    print('Stars: ', repo_dict['stargazers_count'])
-    print('Repository: ', repo_dict['html_url'])
-    print('Created: ', repo_dict['created_at'])
-    print('Updated: ', repo_dict['updated_at'])
-    print('Description: ', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Построение визуализации
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Tetris Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
+
